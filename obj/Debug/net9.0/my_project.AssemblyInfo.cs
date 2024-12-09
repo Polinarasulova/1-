c4e2ecf5536cf0cba0425fd8a9fa92333a64e045
Polina <serysovap@gmail.com>
2024-12-09 22:08:25 +0500

    Merge branch 'master' into bugfix-branch

--- obj/Debug/net9.0/my_project.AssemblyInfo.cs
+++ obj/Debug/net9.0/my_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843a4a23a5f2e34c6cba0f2b23238bd0d11c28df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d03ce4d9a2220ed2c4d6587c07bcfd044533ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("my_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ op/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Выберите действие:");
+        Console.WriteLine("1. Простой калькулятор");
+        Console.WriteLine("2. Угадай число");
+        Console.Write("Введите номер :");
+        
+        string choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+            Calculator calculator = new Calculator();
+            calculator.Run();
+        }
+        else if (choice == "2")
+        {
+            GuessGame guessGame = new GuessGame();
+            guessGame.Run();
+        }
+        else
+        {
+            Console.WriteLine("ТЫ ,что СОВСЕМ.");
+        }
+    }
+}
